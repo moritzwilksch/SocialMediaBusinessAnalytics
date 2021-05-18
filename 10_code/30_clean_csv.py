9902ab9f3cc57a37aff34ad59cc5b7075c21ed8f
Moritz Wilksch <moritzwilksch@gmail.com>
2021-05-18 10:46:06 +0200

    clean parquet export

--- 10_code/30_clean_csv.py
+++ 10_code/30_clean_csv.py
@@ -1,7 +1,12 @@
 # %%
 import pandas as pd
-tickers = ["TSLA", "AAPL", "AMZN", "FB", "MSFT", "TWTR", "AMD", "NFLX", "NVDA", "INTC"]
-# %%
+from rich.console import Console
+c = Console()
+
+root_path = "../"
+tickers = [#"TSLA", 
+"AAPL", "AMZN", "FB", "MSFT", "TWTR", "AMD", "NFLX", "NVDA", "INTC"]
+
 keep_cols = ['id',
              'created_at',
              'date',
@@ -11,8 +16,6 @@ keep_cols = ['id',
              'name',
              'tweet',
              'language',
-             'mentions',
-             'urls',
              'replies_count',
              'retweets_count',
              'likes_count',
@@ -21,21 +24,44 @@ keep_cols = ['id',
              'link']
 
 
-# %%
-ticker = tickers[0]
-df = pd.read_csv(
-    f"../00_source_data/{ticker}_tweets.csv",
-    usecols=keep_cols
-)
-
-# %%
+for ticker in tickers:
+    c.print(f"[LOADING] ${ticker}...", style='white on blue')
+    df = pd.read_csv(
+        root_path + f"00_source_data/{ticker}_tweets.csv",
+        usecols=keep_cols
+    )
 
 
-def fix_dtypes(data: pd.DataFrame) -> pd.DataFrame:
-    data = data.assign(created_at=data.created_at.astype('datetime64'))
+    def fix_dtypes(data: pd.DataFrame) -> pd.DataFrame:
+        c.print(f"[PROCESSING] Fixing dtypes...", style='white on blue')
+        data = data.assign(created_at=pd.to_datetime(data.date + " " + data.time))
+        data = data.drop(['date', 'time'], axis=1)
+
+        string_cols = ['username', 'name', 'tweet', 'link']
+        data[string_cols] = data[string_cols].astype('string')
+
+        data['language'] = data['language'].astype('category')
+        return data
 
 
-(
-    df
-    .pipe(fix_dtypes)
-).info()
+    def clean_object_cols(data: pd.DataFrame) -> pd.DataFrame:
+        c.print(f"[PROCESSING] Cleaning obj-cols...", style='white on blue')
+        data['hashtags'] = data.hashtags.apply(lambda x: ", ".join(x.split("', '")).strip("[']")).astype('string')
+        data.loc[data.hashtags.str.len() == 0, 'hashtags'] = pd.NA
+
+        data['cashtags'] = data.cashtags.apply(lambda x: ", ".join(x.split("', '")).strip("[']")).astype('string')
+        data.loc[data.cashtags.str.len() == 0, 'cashtags'] = pd.NA
+
+        return data
+
+
+    clean: pd.DataFrame = (
+        df
+        .pipe(fix_dtypes)
+        .pipe(clean_object_cols)
+    )
+
+    c.print(f"[SAVING] To parquet...", style='white on blue')
+    clean.to_parquet(root_path + f"20_outputs/parquets/{ticker}_tweets.parquet")
+    c.print(f"[DONE]", style='white on green')
+    del df
